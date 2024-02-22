fa1a00d0111656fc4c1d38b00eb41257fd0f093a
Maite Capistrano Refosco <maite.refosco@eldorado.org.br>
2024-02-22 08:47:38 -0300

    api connected to db

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"github.com/maitecr/api-go-gin/database"
 	"github.com/maitecr/api-go-gin/models"
 	"github.com/maitecr/api-go-gin/routes"
 )
 
 func main() {
+	database.ConectDB()
 	models.Alunos = []models.Aluno{
 		{Nome: "nome", CPF: "515465", RG: "1556454"},
 	}
